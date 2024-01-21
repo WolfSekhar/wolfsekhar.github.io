6c409370329171039eb71d4139b9baa1093bfd35
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-01-22 00:58:55 +0530

    Added an atom animation to welcome page

--- index.js
+++ index.js
@@ -1,82 +1,106 @@
-let container;
-let canvas;
 
-let position;
-let velocity;
-let acceleration;
-
-let step;
-let colors;
-let dot_radius;
-let cindex;
-
-let mySubNameWidth;
-let mySubNameHeight;
+var sketchTwo = function (p) {
+    p.setup = function () {
+        p.container = document.getElementById('dot');
+        p.mySubNameId = document.getElementById('my-name-sub');
+        p.mySubNameHeight = p.mySubNameId.offsetHeight;
+        p.mySubNameWidth = p.mySubNameId.offsetWidth;
 
 
-function setup() {
-    
-    container = document.getElementById('dot');
-    let mySubNameId = document.getElementById('my-name-sub');
-    mySubNameHeight = mySubNameId.offsetHeight;
-    mySubNameWidth = mySubNameId.offsetWidth;
+        p.canvas = p.createCanvas(170, p.mySubNameHeight / 1.2);
+        p.background(255);
+        p.canvas.parent(p.container);
+        p.frameRate(60);
 
 
-    canvas = createCanvas(170,mySubNameHeight/1.2);
-    background(255);
-    canvas.parent(container);
-    frameRate(60);
+        p.position = p.createVector(0, 0);
+        p.position.set(p.width / 6, p.height / 2);
 
+        p.velocity = p.createVector(2, 0.1);
 
-    position = createVector(0,0);
-    position.set(width/6,height/2);
+        p.acceleration = p.createVector(0, 0.1);
 
-    velocity = createVector(2,0.1);
+        p.colors = ['black', '#7851a9', 'orange', '#1e90ff']
+        p.dot_radius = 25;
 
-    acceleration = createVector(0,0.1);
+        p.cindex = 0;
 
-    colors = ['black','#7851a9','orange','#1e90ff']
-    dot_radius = 25;
-
-    cindex = 0;
-    
-}
-
-function draw(){
-    background(255);
-
-    dot(position.x,position.y,dot_radius,colors[cindex])
-
-    motion(position);
-
-    if(position.y > height - (dot_radius/2)){
-        velocity.set(velocity.x,velocity.y * -1);
-        changeColor();
-    }if(position.x > (width - (dot_radius/2)) || position.x < (0 + (dot_radius/2))){
-        velocity.set(velocity.x * -1,velocity.y);
     }
 
+    p.draw = function () {
+        p.background(255);
 
-}
+        dot(p.position.x, p.position.y, p.dot_radius, p.colors[p.cindex])
 
-function dot(x,y,radius,color){
-    fill(color);
-    noStroke();
-    circle( x,y,radius);
-}
+        motion();
 
-function motion(position){
-    position = position.add(velocity); // velocity
-    velocity = velocity.add(acceleration); // acceleration
+        if (p.position.y > p.height - (p.dot_radius / 2)) {
+            p.velocity.set(p.velocity.x, p.velocity.y * -1);
+            changeColor();
+        } if (p.position.x > (p.width - (p.dot_radius / 2)) || p.position.x < (0 + (p.dot_radius / 2))) {
+            p.velocity.set(p.velocity.x * -1, p.velocity.y);
+        }
 
-    // limiting vector magnitude
-    velocity.limit(4);
-}
 
-function changeColor(){
-    if(cindex < colors.length - 1){
-        cindex = cindex + 1;
-    }else{
-        cindex = 0;
+    }
+
+    function dot(x, y, radius, color) {
+        p.fill(color);
+        p.noStroke();
+        p.circle(x, y, radius);
+    }
+
+    function motion() {
+        p.position = p.position.add(p.velocity); // p.velocity
+        p.velocity = p.velocity.add(p.acceleration); // acceleration
+
+        // limiting vector magnitude
+        p.velocity.limit(4);
+    }
+
+    function changeColor() {
+        if (p.cindex < p.colors.length - 1) {
+            p.cindex = p.cindex + 1;
+        } else {
+            p.cindex = 0;
+        }
     }
 }
+
+let s1 = new p5(sketchTwo);
+
+var sketchOne = function (p) {
+
+    p.setup = function () {
+        p.colors = ['#3498DB', '#16A085', '#F39C12', '#D35400', '#8E44AD'];
+        p.container = document.getElementById('welcome-anim');
+        p.canvas = p.createCanvas(220,220);
+        p.canvas.parent(p.container);
+        p.angle = 0;
+        p.step = 0.1;
+        p.positionDevider = 1;
+        p.frameRate(60);
+    }
+
+    p.draw = function () {
+        p.background(255);
+
+        p.translate(p.width / 2, p.height / 2);
+
+        p.fill('#34495E ');
+        p.circle(0, 0, 30);
+
+        p.noStroke();
+
+        for (let i = 5; i < 10; i++) {
+            p.fill(p.colors[i - 5]);
+            p.rotate(p.radians(p.angle + p.step));
+            p.circle(p.width / i, p.height / i, 15);
+        }
+        p.step = p.step + 1;
+    }
+
+}
+
+let s2 = new p5(sketchOne);
+
