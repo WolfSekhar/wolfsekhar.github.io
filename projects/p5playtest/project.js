b0fa04d1330bc4044017cba3b5986082588a6516
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-02-21 15:50:35 +0530

    p5 libraries added and p5play also

---
+++ projects/p5playtest/project.js
@@ -0,0 +1,18 @@
+
+
+function setup(){
+    new Canvas(400,400);
+    
+    let msquare = new Sprite();
+    
+
+
+    msquare.width = 100;
+    msquare.color = 'blue';
+    msquare.rotation = 45;
+    
+}
+
+function draw(){
+    background(255);
+}
