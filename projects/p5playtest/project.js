322ae7d35663f7f6974897766f6a38a526c1b65b
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-03-12 14:18:22 +0530

    added gitignore

--- projects/p5playtest/project.js
+++ projects/p5playtest/project.js
@@ -1,12 +1,33 @@
+let canvas;
+let ball;
+let ground;
 
 
 function setup(){
-    new Canvas(500,450);
-   
+    canvas = new Canvas(500,450);
+
+
+    world.gravity.y = 1;
+
+    ball = new Sprite();
+    ball.diameter = 50;
+
+    ground = new Sprite();
+    ground.width = 1000;
+    ground.height = 2;
+    ground.position.y = height;
+    ground.collider = 's';
+    
+    
     
 }
 
 function draw(){
+    background(255);
+
+    if(kb.presses(' ')){
+        ball.position.y += -100;
+    }
     
 }
 
