b417d516cf605ce45e1c4ae924afc62ebe01e4bd
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-03-24 20:15:54 +0530

    added a cute curve to SHM

--- projects/simpleharmonic/project.js
+++ projects/simpleharmonic/project.js
@@ -6,47 +6,59 @@ let force;
 let friction;
 let position;
 
+let pg;
+
+let counter = 0;
+let myCurve = [];
 
 function setup(){
-    canvas = new Canvas(windowWidth,windowHeight/1.6);
+    canvas = new Canvas(windowWidth - 30,windowHeight/1.6);
+    pg = createGraphics(windowWidth - 30,height/2);
+    pg.background(0);
     frameRate(60);
-    extremeFound = false;
 
     position = createVector(0,0);
     velocity = createVector(0,0);
     acceleration = createVector(0,0);
-    force = createVector(4,0);
+    force = createVector(5,0);
     friction = createVector(0.1,0);
     
 
     object = new Sprite();
     object.diameter = 20;
     object.x = object.y = 0;
-    object.color = 'blue';
+    
 
     acceleration.add(force);
     
-
+    
 }
 
 function draw(){
     background(255);
+    //pg.background(0);
+    translate(width/2,height/4);
 
-    translate(width/2,height/2);
+
+
+
     fill(0);
-
-    
+    stroke(1);
+    strokeWeight(0.5);
     text("Mean position",-30,50);
     text("Maximum Velocity ",-30,30);
-    circle(0,0,10);
+    circle(0,0,5);
+    fill(255);
 
-
-    text('a :' + round(acceleration.x,2),object.x - 30,-30);
+    text('a: ' + round(acceleration.x,2),object.x - 30,-30);
 
     move();
     line(0,0,0,-height/2);
+    drawCurve();
+
     
-}
+    
+} 
 
 function move(){
     velocity.set(0,0);
@@ -68,6 +80,22 @@ function move(){
     
 }
 
-function applyForce(){
+function drawCurve(){
+    push();
+    translate(-width/2,80);
+    pg.fill(255);
+    pg.strokeWeight(1);
+    pg.stroke(255);
+    pg.line(width/2,0,width/2,height);
+    pg.noStroke();
+    if(counter < 300){ // using to control uneccessary out of  screen rendering 
+        
+        pg.circle(object.x + width/2,counter,5);
+        counter += 0.5;
+
+    }
+    image(pg,0,0);
+    pop();
     
 }
+
