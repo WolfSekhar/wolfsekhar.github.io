92add19dec3aca2262bcd69295ff597a379fec7c
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-03-24 18:48:05 +0530

    changed object size

--- projects/simpleharmonic/project.js
+++ projects/simpleharmonic/project.js
@@ -8,19 +8,19 @@ let position;
 
 
 function setup(){
-    canvas = new Canvas(windowWidth-50,windowHeight/1.6);
+    canvas = new Canvas(windowWidth,windowHeight/1.6);
     frameRate(60);
     extremeFound = false;
 
     position = createVector(0,0);
     velocity = createVector(0,0);
     acceleration = createVector(0,0);
-    force = createVector(5,0);
+    force = createVector(4,0);
     friction = createVector(0.1,0);
     
 
     object = new Sprite();
-    object.diameter = 50;
+    object.diameter = 20;
     object.x = object.y = 0;
     object.color = 'blue';
 
@@ -41,7 +41,7 @@ function draw(){
     circle(0,0,10);
 
 
-    text('Acceleration :' + round(acceleration.x,2),object.x,-30);
+    text('a :' + round(acceleration.x,2),object.x - 30,-30);
 
     move();
     line(0,0,0,-height/2);
